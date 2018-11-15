8e982c214ad249914fbdb031a79c19aaa7c25ef3
Shane <Shane@DESKTOP-3A8FODV>
2018-11-15 11:42:34 +0000

    added loop and checks name

--- exercise1/Program.cs
+++ exercise1/Program.cs
@@ -7,9 +7,31 @@ namespace exercise1
         static void Main(string[] args)
         {
             Console.WriteLine("Enter name");
-            string name = Console.ReadLine();
+            string name = Console.ReadLine().ToLower();
+            Console.ReadKey();
 
-            string[] names = new string[4];
+            string[] names = new string[3];
+            names[0] = "shane";
+            names[1] = "bob";
+            names[2] = "fred";
+            bool isName = false;
+
+            for(int i = 0; i < names.Length; i++ )
+            {
+                if(name == names[i])
+                {
+                    isName = true;
+                }
+            }
+
+            if(isName == true)
+            {
+                Console.WriteLine("Your name is in the list");
+            }
+            else
+            {
+                Console.WriteLine("Name isn't on list");
+            }
 
 
         }
